80a09aa6ba7cb9b9bffb510552c17638c7401c1a
郭梦迪 <yjzty222@qq.com>
2026-04-07 20:52:01 +0800

    48869. 小郑的蓝桥平衡串(前缀和+字符串)

---
+++ 前缀和2.cpp
@@ -0,0 +1,29 @@
+//48869. 小郑的蓝桥平衡串(前缀和+字符串)
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+vector<int> a;
+int main() {
+    cin>>s;
+    int len=s.size();
+    a.resize(len+1);
+    for(int i=0;i<len;i++){
+        if(i==0){
+            a[0]=(s[0]=='L')?1:-1;
+        }else{
+            a[i]=a[i-1]+((s[i]=='L')?1:-1);
+        }
+    }
+    int ans=0;
+    for(int i=0;i<len;i++){
+        for(int j=i+1;j<len;j++){
+            if(i==0&&a[j]==0) ans=max(ans,j+1);
+            else if(a[j]-a[i-1]==0)
+              ans=max(ans,j-i+1);
+        }
+    }
+    cout<<ans;
+    return 0;
+}
+
+//1 0 1 2
