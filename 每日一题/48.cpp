53c2c4b2105d27b4b21d3fcc9e8bf3a68575338e
郭梦迪 <yjzty222@qq.com>
2026-08-04 20:11:21 +0800

    Create 48.cpp

---
+++ 每日一题/48.cpp
@@ -0,0 +1,9 @@
+//877. 石子游戏
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return 1;
+    }
+};
+解释:这道题的设置带来的有趣点是,Alice第一次拿走的是第一个,那么她拿的总是偶数序号的.反之,Alice第一次拿走左后一个,那么她拿的总是奇数序号的.
+  Alice只要事先计算奇数和大还是偶数和大即可
