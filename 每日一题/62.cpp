8b194b43519d58653f47add1123f86ec0d2a516a
郭梦迪 <yjzty222@qq.com>
2026-09-20 15:41:13 +0800

    Add solution for reverse degree of a string

---
+++ 每日一题/62.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.cn/problems/reverse-degree-of-a-string/
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+            ans+=(26-(s[i]-'a'))*(i+1);
+            
+        return ans;
+    }
+};
